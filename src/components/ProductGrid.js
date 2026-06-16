6e0369cf4699d338f109d91270c03746a782249b
dj <saini26daljit@gmail.com>
2026-06-15 23:50:19 -0500

    Add product card and product grid components

---
+++ src/components/ProductGrid.js
@@ -0,0 +1,20 @@
+import ProductCard from './ProductCard';
+
+function ProductGrid(props) {
+    return (
+        <section className="product-grid">
+            {
+                props.products.map(product => (
+                    <ProductCard
+                        key={product.id}
+                        title={product.title}
+                        price={product.price}
+                        image={product.image}
+                    />
+                ))
+            }
+        </section>
+    );
+}
+
+export default ProductGrid;
