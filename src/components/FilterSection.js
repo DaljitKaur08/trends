cbfd979665390d11ac71b730559fcd76de0f6b03
dj <saini26daljit@gmail.com>
2026-06-16 00:07:43 -0500

    filter section

---
+++ src/components/FilterSection.js
@@ -0,0 +1,15 @@
+function FilterSection() {
+    return (
+        <section className="filter-section">
+            <select>
+                <option value="all">All Categories</option>
+                <option value="men's clothing">Men's Clothing</option>
+                <option value="women's clothing">Women's Clothing</option>
+                <option value="jewelery">Jewelry</option>
+                <option value="electronics">Electronics</option>
+            </select>
+        </section>
+    );
+}
+
+export default FilterSection;
