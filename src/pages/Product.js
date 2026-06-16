1adcd3f246659166231b57edeebc67e60fc69bfe
dj <saini26daljit@gmail.com>
2026-06-16 00:17:31 -0500

    Display products on product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import FilterSection from '../components/FilterSection';
 import SortSection from '../components/SortSection';
+import ProductGrid from '../components/ProductGrid';
 
 function Product() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+        axios
+            .get('https://fakestoreapi.com/products')
+            .then(res => {
+                setProducts(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+    }, []);
+
     return (
         <>
             <h2>Products</h2>
@@ -9,6 +29,8 @@ function Product() {
             <FilterSection />
 
             <SortSection />
+
+            <ProductGrid products={products} />
         </>
     );
 }
