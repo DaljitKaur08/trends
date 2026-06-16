0d6e46d7be7f115ae4213013daa92c00bab0b7cc
dj <saini26daljit@gmail.com>
2026-06-15 23:52:39 -0500

    Connect FakeStore API to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,30 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import HeroBanner from '../components/HeroBanner';
+import ProductGrid from '../components/ProductGrid';
 
 function Home() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+        axios
+            .get('https://fakestoreapi.com/products')
+            .then(response => {
+                setProducts(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+    }, []);
+
     return (
         <>
             <HeroBanner />
+            <ProductGrid products={products} />
         </>
     );
 }
