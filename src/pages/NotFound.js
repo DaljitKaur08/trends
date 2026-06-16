6c8d73d0774a5d5eadf19d94d30ad22579538502
Harpreet <ksfibri@gmail.com>
2026-06-16 18:18:28 -0500

    Styling the cart page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom';
 function NotFound() {
     return (
         <section className="not-found">
-            <h1>404</h1>
-            <h2>Page Not Found</h2>
-            <p>The page you are looking for does not exist.</p>
 
-            <Link to="/" className="btn">
+            <h1>404</h1>
+
+            <h2>Page Not Found</h2>
+
+            <p>
+                Sorry, the page you are looking for
+                does not exist.
+            </p>
+
+            <Link to="/">
                 Back To Home
             </Link>
+
         </section>
     );
 }
