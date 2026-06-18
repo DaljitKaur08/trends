67376a7c36b24de4764a2cca74e212dd017fb7fe
Harman <harmandeepsingh7@student.mitt.ca>
2026-06-18 10:41:11 -0500

    added content to the footer

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
+function Footer() {
+    return (
+        <footer className="footer">
+
+            <div className="footer-container">
+
+                <div className="footer-section">
+                    <h2 className="logo">Trends</h2>
+                    <p>123 Shopping Street</p>
+                    <p>Winnipeg, Canada</p>
+                    <p>+1 234 567 890</p>
+                    <p>support@trends.com</p>
+
+                    <div className="social-icons">
+                        <FaFacebookF />
+                        <FaInstagram />
+                        <FaTwitter />
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div className="footer-bottom">
+                <p>© 2026 Trends. All rights reserved.</p>
+            </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
